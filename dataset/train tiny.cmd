26d8aacc8c8aad5eb07c253ce7df8d6ff3530165
Josue Lopez <josuelopezv@gmail.com>
2021-06-27 16:26:38 -0600

    .

---
+++ dataset/train tiny.cmd
@@ -0,0 +1,4 @@
+# py create_list_of_images.py
+# ..\darknet\darknet.exe detector train data\obj.data data\yolov4-tiny-custom.cfg yolov4-tiny.conv.29
+..\darknet\darknet.exe detector train data\obj.data data\yolov4-tiny-3l.cfg yolov4-tiny.conv.29
+pause
---
+++ dataset/train.cmd
@@ -0,0 +1,3 @@
+py create_list_of_images.py
+..\darknet\darknet.exe detector train data\obj.data data\yolo-obj.cfg yolov4.conv.137
+pause
