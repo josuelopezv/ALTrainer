8fea7df2f7b1b3f665b8b81cf7bb184f26637aff
Josue Lopez <josuelopezv@gmail.com>
2021-06-28 10:29:46 -0600

    .

---
+++ dataset/test w.cmd
@@ -0,0 +1,4 @@
+# ..\darknet\darknet.exe detector map data\obj.data data\yolov4-tiny-3l.cfg backup\yolov4-tiny-3l_3000.weights
+..\darknet\darknet.exe detector map data\obj.data data\yolov4-tiny-3l.cfg backup\yolov4-tiny-3l_final.weights
+
+pause
