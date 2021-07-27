f8c6c0adfacbdc51bda24b09f0dc6c1e8de4f92a
Josue Lopez <josuelopezv@gmail.com>
2021-07-26 22:33:13 -0600

    .

--- dataset/train.cmd
+++ dataset/train.cmd
@@ -1,4 +1,4 @@
-del ".\backup"
+del ".\backup" /Q
 python create_list_of_images.py
 ..\darknet\darknet.exe detector train data\obj.data data\yolov4-tiny-3l.cfg yolov4-tiny.conv.29
 copy data\yolov4-tiny-3l.cfg C:\FAB\tf\yolov4-tiny-3l.cfg /Y
